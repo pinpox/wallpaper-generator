61932953ce2d3b3155a171dcd1781fe1fea932e6
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-12-30 14:31:46 +0100

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,27 +1,25 @@
 # Import the nix package collection, so we can access the `pkgs` and `stdenv`
 # variables
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 # List lua packages sepraterly, so we can get the path in the exports
 with luaPackages;
 let
-  libs = [lua lgi luafilesystem];
-in
+  libs = [ lua lgi luafilesystem ];
 
-# Make a new "derivation" that represents our shell
-stdenv.mkDerivation rec {
+  # Make a new "derivation" that represents our shell
+in stdenv.mkDerivation rec {
   name = "lua-env";
 
   # The packages in the `buildInputs` list will be added to the PATH in our shell
-  buildInputs = [
-    entr
-    feh
-  ] ++ libs;
+  buildInputs = [ entr feh ] ++ libs;
 
   # Export paths and add helper functions and aliases
   shellHook = ''
     export LUA_CPATH="${lib.concatStringsSep ";" (map getLuaCPath libs)}"
-    export LUA_PATH="${lib.concatStringsSep ";" (map getLuaPath libs)};$LUA_PATH;$(pwd)/?.lua"
+    export LUA_PATH="${
+      lib.concatStringsSep ";" (map getLuaPath libs)
+    };$LUA_PATH;$(pwd)/?.lua"
 
     preview() {
       echo main.lua | entr sh -c 'lua main.lua' &
