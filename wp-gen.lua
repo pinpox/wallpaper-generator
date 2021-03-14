2c4e6e8ba70dbcc50dd32d2f7712b2a89102896e
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-15 00:41:09 +0100

    fix path for generators

--- wp-gen.lua
+++ wp-gen.lua
@@ -4,12 +4,17 @@ local M = {}
 -- Luafilesystem allows to iterate over a directory.
 local Lfs = require "lfs"
 
+local folderOfThisFile = (...):match("(.-)[^%.]+$")
+
 -- for each filename in the directory
-for filename in Lfs.dir "generators/" do
+for filename in Lfs.dir (folderOfThisFile .. "generators/" )do
 	-- if it is a file
-	if Lfs.attributes ("generators/" .. filename, "mode") == "file" then
+	if Lfs.attributes (folderOfThisFile .. "generators/" .. filename, "mode") == "file" then
+
+
+
 		-- transform the filename into a module name
-		local name = "generators/" .. filename
+		local name = folderOfThisFile .. "generators/" .. filename
 		name = name:sub (1, #name-4)
 		name = name:gsub ("/", ".")
 		print(name)
